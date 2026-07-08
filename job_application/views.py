4b4864f3ce3a4769bc71496f30deb17827831879
AndrewTechTips <andreww.steel69@gmail.com>
2026-07-08 18:28:24 +0300

    Rename model 'From' to 'Form' and update form handling in index view

--- job_application/views.py
+++ job_application/views.py
@@ -1,12 +1,26 @@
 from django.shortcuts import render
-from forms import ApplicationForm
+from .forms import ApplicationForm
+from .models import Form
 
 
 def index(request):
     if request.method == "POST":
-        form = ApplicationForm()
-        first_name = form.cleaned_data.get("first_name", "")
-        last_name = form.cleaned_data.get("last_name", "")
-        email = form.cleaned_data.get("email", "")
-        occupation = form.cleaned_data.get("occupation", "")
+        form = ApplicationForm(request.POST)
+
+        if form.is_valid():
+            first_name = form.cleaned_data.get("first_name", "")
+            last_name = form.cleaned_data.get("last_name", "")
+            email = form.cleaned_data.get("email", "")
+            date = form.cleaned_data.get("date", "")
+            occupation = form.cleaned_data.get("occupation", "")
+
+            # Store data in the db
+            Form.objects.create(
+                first_name=first_name,
+                last_name=last_name,
+                email=email,
+                date=date,
+                occupation=occupation,
+            )
+
     return render(request, "index.html")
