a3276a68fe4731d8a1a57330b069ea85838f4030
AndrewTechTips <andreww.steel69@gmail.com>
2026-07-08 17:55:26 +0300

    Add application form and integrate into index view

--- job_application/views.py
+++ job_application/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from forms import ApplicationForm
 
 
 def index(request):
+    if request.method == "POST":
+        form = ApplicationForm()
+        first_name = form.cleaned_data.get("first_name", "")
+        last_name = form.cleaned_data.get("last_name", "")
+        email = form.cleaned_data.get("email", "")
+        occupation = form.cleaned_data.get("occupation", "")
     return render(request, "index.html")
