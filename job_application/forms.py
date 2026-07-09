9e1d240febd9fc0fa552ee5e91869de18703d7bf
AndrewTechTips <andreww.steel69@gmail.com>
2026-07-09 14:59:25 +0300

    Enhance JobApplication form: add custom widgets for improved user experience

--- job_application/forms.py
+++ job_application/forms.py
@@ -6,3 +6,17 @@ class JobApplicationForm(forms.ModelForm):
     class Meta:
         model = JobApplication
         fields = ["first_name", "last_name", "email", "date", "occupation"]
+
+        widgets = {
+            "first_name": forms.TextInput(
+                attrs={"class": "form-control", "placeholder": "John"}
+            ),
+            "last_name": forms.TextInput(
+                attrs={"class": "form-control", "placeholder": "Doe"}
+            ),
+            "email": forms.EmailInput(
+                attrs={"class": "form-control", "placeholder": "john.doe@example.com"}
+            ),
+            "date": forms.DateInput(attrs={"class": "form-control", "type": "date"}),
+            "occupation": forms.RadioSelect(attrs={"class": "form-check-input"}),
+        }
