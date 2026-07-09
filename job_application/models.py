2aad5ff1acb0140732844cfccc6d0725d12bdece
AndrewTechTips <andreww.steel69@gmail.com>
2026-07-09 14:57:08 +0300

    Add occupation choices to JobApplication model with default value

--- job_application/models.py
+++ job_application/models.py
@@ -2,11 +2,23 @@ from django.db import models
 
 
 class JobApplication(models.Model):
+
+    class OccupationChoices(models.TextChoices):
+        EMPLOYED = "employed", "Employed"
+        UNEMPLOYED = "unemployed", "Unemployed"
+        SELF_EMPLOYED = "self-employed", "Self-Employed"
+        STUDENT = "student", "Student"
+
     first_name = models.CharField(max_length=80)
     last_name = models.CharField(max_length=80)
     email = models.EmailField()
     date = models.DateField()
-    occupation = models.CharField(max_length=80)
+
+    occupation = models.CharField(
+        max_length=20,
+        choices=OccupationChoices.choices,
+        default=OccupationChoices.STUDENT,
+    )
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
